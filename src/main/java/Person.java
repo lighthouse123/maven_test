ee03a7c9198a4fc51268b8464654c972a3d8b815
范成毅 <fanchengyi_kk@sina.com>
2022-03-21 09:56:28 +0800

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,38 @@
+/**
+ * @description:
+ * @author: fan
+ * @date: 2022/3/21 9:34
+ * @version: 1.0
+ */
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
